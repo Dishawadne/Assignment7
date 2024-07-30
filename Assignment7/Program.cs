264bb74132530349c54e657b1c6d947845a5e583
Dishawadne <wadnedisha776@gmail.com>
2024-07-30 16:15:31 +0530

    Add project files.

---
+++ Assignment7/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignment7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] A = [ -6, 5, 2,-4];
+             //int[] A = [-1, -3];
+            //int[] A = [1, 2, 3];
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(A));
+        
+    }
+    }
+}
---
+++ Assignment7/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7
+{
+    internal class Solution
+    {
+        public int solution(int[] A)
+        {
+            int minPositive = int.MaxValue;
+            bool foundPositive = false;
+
+            foreach (int num in A)
+            {
+                if (num > 0)
+                {
+                    minPositive = Math.Min(minPositive, num);
+                    foundPositive = true;
+
+                }
+            }
+
+            if (!foundPositive)
+            {
+                return 0;
+            }
+
+            return minPositive * minPositive;
+
+
+
+        }
+    }
+}
